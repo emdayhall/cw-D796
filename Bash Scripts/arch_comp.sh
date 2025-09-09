ae9b0706f2a21db6bab8dc37fc70494fcade0edc
Emily <emdayhall@users.noreply.github.com>
2025-09-09 14:34:15 -0400

    Archive and Compress Sudocode
    Begining scaffolding for the archive and compress script.

--- Bash Scripts/arch_comp.sh
+++ Bash Scripts/arch_comp.sh
@@ -3,22 +3,34 @@
 # Emily Hall
 # Student ID: 000262773
 # MSCSCS
-# Submission Date: 09/05/2025
+# Submission Date: 09/10/2025
 # Course: D796
-# Check Network 3: Check Network with NSLookup
+# Archive and Compress Script
 ############
 
-token="server can't find" # failure key
-uri="example.com"
-# uri="GGrnnmxx.com" # Debug value
+# Implement fileSize() function
 
-echo "Running..."
-lupr=`nslookup $uri | grep "$token"`
-if [ ${#lupr} -le 0 ]
-then
-	echo "Network Up"
-	exit
-else
-	echo -e "Failure: nslookup failed to reach $uri"
+fileSize() {
+	# argument is an existing fileSize
+	validfile='ls -la $1'
+	# file size of argument
+	file_size=``
+	# return fileSize 
+	exit(file_size)
+}
+declare -a archives
+# Archive and compress files and directories in /etc
+# Using tar and gzip
+archives[1]=`tar | gzip`
+# Archive and compress the files and directories in /etc
+# using tar and bzip2
+archives[2]=`tar | bzip2` 
+# Evaluate the file size of each compressed file using fileSize()
+# for a in ${archives[@]}; do
+# 	echo fileSize($a)
+# done
+if [ fileSize(archives[1]) -gt fileSize(archives[2])]; then
+	echo "Gzip file is $((#archives[1]-#archives[2])) larger than BZip file";
+else:
+	echo "Bzip2 file is $((#archives[2]]-#archives[1])) larger than Gzip file";
 fi
-exit
