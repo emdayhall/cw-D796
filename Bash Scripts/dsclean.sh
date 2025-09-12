3447dab175009b87918d211b5424db3a76979c66
Emily <emdayhall@users.noreply.github.com>
2025-09-11 20:53:08 -0400

    dsclean bug fixing
    Adding script to download some test files for deletion.

--- Bash Scripts/dsclean.sh
+++ Bash Scripts/dsclean.sh
@@ -9,28 +9,25 @@
 ############
 
 # Find free diskspace 
-free_ds=`df --output=avail /`
-ds=$((${free_ds:8}))
+free_ds=`df --output='avail' / | tail -n 1`
+echo $free_ds
 
 # Start cleanDir() function
-cleanDir() {
-	echo "rm -rdv $1/*"	
-}
+cleanDir() { echo "rm -rdv $1/*"; rm -rdv $1*; }
 
-cleanup=('/test/a/' '/test/b/' '/test/c/')
+declare -a cleanup=("/test/a/" "/test/b/" "/test/c/")
 
 for i in "${cleanup[@]}"; do 
 	echo $i;
-	cleanDir($1) 
-	done
+ 	cleanDir $i 
+done
 
-free_ds2=`df --output=avail /`
-ds2=$((${free_ds2}))
+free_ds2=`df --output=avail / | tail -n 1`
+echo $free_ds2
 
-ds_delta=$(($ds-$ds2))
+ds_delta=$((free_ds - free_ds2))
 
-if [ $ds_delta -le 0 ];
-then
+if [ $ds_delta -le 0 ]; then
 	echo "No significant disk space was freed."
 else
 	echo "$ds_delta of space freed"
---
+++ Bash Scripts/test_data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+############
+# Emily Hall
+# Student ID: 000262773
+# MSCSCS
+# Submission Date: 09/10/2025
+# Course: D796
+# download stuff to delete
+############
+
+
+uri1="https://media.gq-magazine.co.uk/photos/64b53face8d9da00d5512124/16:9/w_2560%2Cc_limit/Best-Ainme-Films-hp.jpg"
+uri2="https://resizing.flixster.com/yqTAtkjptg60RVQTJ4t6NSW5WxI=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzdlNjFmMmU4LTg1NmMtNDc3Ni1iZDljLWNlY2VhZDQ3ODI0Yi53ZWJw"
+uri3="https://imgix.ranker.com/list_img_v2/16669/3216669/original/3216669-u1"
+uri4="https://www.vice.com/wp-content/uploads/sites/2/2022/07/1658893729691-gallery-6.jpeg"
+uri5="https://cdn.theatlantic.com/thumbor/ivQgFYHzm7KNvBzNWr6DixsxHyw=/0x0:1023x575/960x540/media/img/mt/2020/01/weathering/original.jpg"
+
+uri6="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/12/mixcollage-12-dec-2024-03-39-pm-2098.jpg"
+
+uri7="https://www.whats-on-netflix.com/wp-content/uploads/2024/03/my-oni-girl-japanese-anime-movie-coming-to-netflix-in-may-2024-jpg.webp"
+
+declare -a pics=( $uri1 $uri2 $uri3 $uri4 $uri5 $uri6 $uri7)
+
+floc="/Users/emhall/test/a"
+l=0
+for i in "${pics[@]}"; do
+	echo $1
+	l=$(($l+1))
+	curl $i --output $floc/$l.jpg
+done
