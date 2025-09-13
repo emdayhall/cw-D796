899f0200c543ff6e154da63a82cc2d9b83ed78b1
Emily <emdayhall@users.noreply.github.com>
2025-09-13 00:34:22 -0400

    Update arch_comp.sh
    Cleaned up and ready for final testing.

--- Bash Scripts/arch_comp.sh
+++ Bash Scripts/arch_comp.sh
@@ -10,32 +10,28 @@
 
 # Implement fileSize() function
 
-fileSize() {
-	# argument is an existing fileSize
-	validfile='ls -la $1'
-	# file size of argument
-	file_size=``
-	# return fileSize 
-	return $file_size
+fileSize() { 
+	vf=`du $1`
+	file_size=${vf:0:$((${#vf}-${#1}))}
+	echo $file_size
 }
 
-# Archive and compress files and directories in /etc
-# Using tar and gzip
+# archive file names
 afn1="etc.tar.gzip"
 afn2="etc.tar.bzip"
-archive1=`tar -czf ~/$afn1 /etc/*`
-# Archive and compress the files and directories in /etc
-# using tar and bzip2
-archive2=`tar -cjfy ~/$afn2 /etc/*` 
-# Evaluate the file size of each compressed file using fileSize()
-# for a in ${archives[@]}; do
-# 	echo fileSize($a)
-# done
-a=fileSize($archive1)
-b=fileSize($archives2)
-if [ $a -gt $b ]; then
-	echo "Gzip file is $(($a - $b)) larger than BZip file";
-else:
-	echo "Bzip2 file is $(($b - $a)) larger than Gzip file";
+
+# Archive and compress the files and directories in /etc using tar andgzip2
+archive1=`tar -czf $afn1 /etc/* &> /dev/null`
+# Archive and compress the files and directories in /etc using tar and bzip2
+archive2=`tar -cjf $afn2 /etc/* &> /dev/null` 
+# Get the file sizes for the archives using fileSize() function
+declare -i a=$(fileSize "$afn1") 
+declare -i b=$(fileSize "$afn2")
+
+# Determine the larger file and report the size delta to the CMD
+if [[ $a -gt $b ]]; then
+	echo "Gzip file is $((a - b)) larger than BZip file";
+else
+	echo "Bzip2 file is $((b - a)) larger than Gzip file";
 fi
 
